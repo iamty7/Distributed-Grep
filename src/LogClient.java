9699f661584a88f8ece8f19d32be7ca349fa68aa
iamty <taoyu1994x@gmail.com>
2017-09-17 11:20:48 -0400

    improved code readability

--- src/LogClient.java
+++ src/LogClient.java
@@ -9,12 +9,27 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
+/*
+ * Interacts with users. 
+ * Scans their inputs of options and patterns for the grep command.
+ * Sends the command to the server.
+ * Displays the data from server.
+ * 
+ * 
+ * @author Yu Tao
+ * 
+ */
 public class LogClient implements Runnable {
+
+	// represents for numbers of child threads that finish tasks
 	static int threadsCnt = 0;
+	// used to store grep results
 	private List<String> grepRet = new LinkedList<String>();
+	// counts line number of the results
 	static int lineCnt = 0;
 
 	public static void main(String[] args) {
+		// starting the main thread
 		new Thread(new LogClient()).start();
 	}
 
@@ -23,37 +38,42 @@ public class LogClient implements Runnable {
 		logClientMain();
 	}
 
+	/*
+	 * Implements functionalities of LogClient
+	 * 
+	 */
 	private void logClientMain() {
+
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			// read grep command
-			// while (!sc.hasNextLine()) {
-			// continue;
-			// }
 
-			System.out.print("Please input your pattern:");
+			// users input options and patterns for grep command
+			System.out.print("Your option & pattern:");
 			String pattern = sc.nextLine();
+
 			if (pattern.equals("exit"))
 				break;
+
 			String cmd = "grep " + pattern;
 
-			// start multithreading
+			// starting multithreading, one thread connecting one VM
 			for (int i = 0; i < 10; ++i) {
 				new Thread(new MultiClient(cmd, i, grepRet)).start();
 			}
 
-			// display grep results from VMs
+			// main thread waiting until all child threads finish their tasks
 			while (true) {
 				if (LogClient.threadsCnt == 10)
 					break;
 				else
 					try {
-						Thread.sleep(500);
+						Thread.sleep(50);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
 			}
-			System.out.println("While Loop Complete");
+
+			// print out grep results
 			for (int i = 0; i < grepRet.size(); ++i) {
 				if (grepRet.get(i) != null && grepRet.get(i).length() != 0) {
 					System.out.println(grepRet.get(i) + "\n");
@@ -61,33 +81,48 @@ public class LogClient implements Runnable {
 			}
 
 			System.out.println(LogClient.lineCnt + " lines found!");
-			//
+
+			// initialize global variants
 			LogClient.threadsCnt = 0;
 			grepRet.clear();
 			lineCnt = 0;
 		}
 		sc.close();
 	}
-
+	
+	
+	/*
+	 * Child thread used to connects with server and sends a grep command
+	 * 
+	 */
 	static class MultiClient implements Runnable {
 
 		private Socket socket;
 		private String host;
 		private int port;
 		private String cmd;
+		// used to indicates the number of the VM
 		private int index;
 		List<String> grepRet;
 
+		/*
+		 * 
+		 * The constructor creates a thread to connect to a server
+		 * 
+		 * @param cmd
+		 * 			the options and pattern of the grep command
+		 * @param index
+		 * 			the index of the child thread, from 0~9
+		 * @param grepRet
+		 * 			the grep results for the main thread to display and store
+		 */
 		public MultiClient(String cmd, int index, List<String> grepRet) {
-			// calculate IP address
+			// calculates IP address
 			StringBuffer sb = new StringBuffer();
 			sb.append("172.22.146.1");
 			sb.append(String.valueOf(index + 42));
 			this.host = sb.toString();
-			// System.out.println(sb.toString());
-
 			this.port = 8399;
-			// specify the log file name
 			this.cmd = cmd + " vm" + String.valueOf(index + 1) + ".log";
 			this.index = index;
 			this.grepRet = grepRet;
@@ -99,21 +134,26 @@ public class LogClient implements Runnable {
 			handleSocket();
 		}
 
+		/*
+		 * Sets up a socket to a server and grabs the results
+		 * 
+		 */
 		private void handleSocket() {
 			try {
-				// set up socket
+				// sets up socket
 				this.socket = new Socket(host, port);
 				Writer writer = new OutputStreamWriter(socket.getOutputStream());
 
-				// send command to the server
+				// sends command to the server
 				writer.write(cmd);
 				writer.flush();
 
-				// read grep results line by line
+				// read grep results line by line and restore to the grepRet
 				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()),
 						10 * 1024 * 1024);
 				StringBuffer sb = new StringBuffer();
 				String string;
+				// counts line number for one VM
 				int vmLineCnt = 0;
 				socket.setSoTimeout(10 * 1000);
 				while ((string = reader.readLine()) != null) {
@@ -124,7 +164,7 @@ public class LogClient implements Runnable {
 
 				grepRet.add(sb.toString());
 
-				// close all
+				// close sockets and I/O streams
 				writer.close();
 				reader.close();
 				socket.close();
--- src/LogServer.java
+++ src/LogServer.java
@@ -1,63 +1,51 @@
 
-/**
- * Created by Jie on 9/12/17.
- */
 import java.io.*;
 import java.net.*;
 
+/*
+ * Listens on 8399 socket and connects with a client
+ * Receives grep command from client and executes the command
+ * Sends results back to the client
+ * 
+ * @author Jie Yin
+ */
+
 public class LogServer {
 	public static void main(String args[]) throws IOException {
-		// throw exception information out
-		int port = 8399;
 		// define port number
-
+		int port = 8399;
+	
 		ServerSocket server = new ServerSocket(port);
 		while (true) {
-
+			// waits for the request from clients
 			Socket socket = server.accept();
-			// waiting for the request from clients
-
+			
+			// reads the command from the client
 			Reader reader = new InputStreamReader(socket.getInputStream());
-			// read the command from the client
 			int len;
 			char chars[] = new char[64];
 			StringBuilder sb = new StringBuilder();
 			len = reader.read(chars);
 			sb.append(chars, 0, len);
-			// put the received string into sb.
 
+			// executes the command
 			Process process = null;
 			process = Runtime.getRuntime().exec(sb.toString());
-			// execute the command line contained in sb.
-
+			
+			// reads results form inputStream and sends back to the client
 			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()), 10 * 1024 * 1024);
-			// put the execution result into br.
-			// StringBuilder outtemp = new StringBuilder();
-			// int linecount = 0;
 			String out_line;
 			Writer output = new OutputStreamWriter(socket.getOutputStream());
 			while ((out_line = br.readLine()) != null) {
-				// ++linecount;
-				// sb.append(outtemp);
-				System.out.println(out_line);
 				output.write(out_line + "\n");
 				output.flush();
 			}
-
-			// if (br.readLine() == null){
-			// outline = outputline.append()
-			// }
-			// put all the lines in br to outtemp.
-
-			// Writer output = new OutputStreamWriter(socket.getOutputStream());
-			// output.write(outtemp.toString());
-			// output.flush();
-			// transmit outtemp to the client
-			// output.close();
+			
+			// close all the resources
 			output.close();
 			reader.close();
 			socket.close();
-			// close all the resources
+			
 		}
 	}
 }
